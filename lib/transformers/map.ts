44013bfc3de214b859c9bf96ed544facf13e8d26
Cole MacKenzie <colemackenzie1@gmail.com>
2022-06-12 14:39:15 -0700

    feat: adding flat map transformation stream

--- lib/transformers/map.ts
+++ lib/transformers/map.ts
@@ -9,3 +9,22 @@ export class MapTransformer<I, O = unknown> implements Transformer {
     controller.enqueue(this.fn(chunk));
   }
 }
+
+export class FlatMapTransformer<I, O = unknown> implements Transformer {
+  fn: (chunk: I) => O;
+
+  constructor(fn: (chunk: I) => O) {
+    this.fn = fn;
+  }
+
+  transform(chunk: I, controller: TransformStreamDefaultController<O>) {
+    const result = this.fn(chunk);
+    if (Array.isArray(result)) {
+      result.forEach((e) => {
+        controller.enqueue(e);
+      });
+    } else {
+      controller.error(new Error("result of map was not an array"));
+    }
+  }
+}
--- tests/map_test.ts
+++ tests/map_test.ts
@@ -1,5 +1,8 @@
-import { MapStream } from "../index.ts";
-import { fail } from "https://deno.land/std@0.143.0/testing/asserts.ts";
+import { FlatMapStream, MapStream } from "../index.ts";
+import {
+  assertEquals,
+  fail,
+} from "https://deno.land/std@0.143.0/testing/asserts.ts";
 
 Deno.test("map stream", async () => {
   const { readable, writable } = new TransformStream();
@@ -22,4 +25,29 @@ Deno.test("map stream", async () => {
     }
     if (result % 10 !== 0) fail("expected everything to be a multiple of 10");
   }
+});
+
+Deno.test("flatmap stream", async () => {
+  const { readable, writable } = new TransformStream();
+  const data = [1, 2, 3, 4, 5];
+  const duplicateStream = new FlatMapStream((a: number) => [a, a]);
+
+  const source = new ReadableStream({
+    start(controller: ReadableStreamDefaultController) {
+      data.forEach((e) => {
+        controller.enqueue(e);
+      });
+      controller.close();
+    },
+  });
+
+  source.pipeThrough(duplicateStream).pipeTo(writable);
+  const results = [];
+  for await (const result of readable) {
+    if (typeof result !== "number") {
+      fail("exptected number but got " + typeof result);
+    }
+    results.push(result);
+  }
+  assertEquals(results, [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
 });
